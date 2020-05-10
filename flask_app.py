ff104ce02623d8e1367a0c5b9176f4febb1c61a0
dc-carrolls <32237935+dc-carrolls@users.noreply.github.com>
2020-05-10 15:36:57 +0000

    Added SQL Database functions

--- flask_app.py
+++ flask_app.py
@@ -1,19 +1,52 @@
 
 # A very simple Flask Hello World app for you to get started with...
 
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, redirect, render_template, request, url_for, current_app, g
+from flask.cli import with_appcontext
+
+import sqlite3
+import click
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-comments = []
+
+DATABASE = '/home/src/mysite/mytest.db'
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def get_comments():
+    cur = get_db().execute('SELECT content FROM comments')
+    rv = cur.fetchall()
+    cur.close()
+    return rv
+
+def set_comments(content):
+    if len(content) != 0:
+#        get_db().execute('INSERT INTO comments(content) VALUES("' + content + '")')
+        get_db().execute('INSERT INTO comments(content) VALUES(?)', (content,))
+        get_db().commit()
+
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", comments=comments)
+        my_comments = get_comments()
+        return render_template("main_page.html", comments=my_comments)
 
-    comments.append(request.form["contents"])
+    set_comments(request.form["contents"])
     return redirect(url_for('index'))
 
 
